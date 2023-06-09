bc36344740f32e973d3b09fad72c932d8c87919b
Urvashi Parmar <parmarurvashi01@gmail.com>
2023-06-09 07:43:19 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CreateTodo from './components/CreateTodo'
+
+function App() {
+  return (
+    <div>
+      App
+      <CreateTodo/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/alert.js
@@ -0,0 +1,21 @@
+// alert.js
+import { enqueueSnackbar } from 'notistack'
+
+function alert(
+  message,
+  alertType,
+) {
+    if (alertType === 'success') {
+      enqueueSnackbar(message, { variant: 'success', anchorOrigin : { vertical: 'top', horizontal: 'right', }, })
+    } else if (alertType === 'error') {
+      enqueueSnackbar(message, { variant: 'error', anchorOrigin : { vertical: 'top', horizontal: 'right', }, })
+    } else if (alertType === 'warning') {
+      enqueueSnackbar(message, { variant: 'warning', anchorOrigin : { vertical: 'top', horizontal: 'right', }, })
+    } else if (alertType === 'info') {
+      enqueueSnackbar(message, { variant: 'info', anchorOrigin : { vertical: 'top', horizontal: 'right', }, })
+    } else {
+      enqueueSnackbar(message, { variant: 'default', anchorOrigin : { vertical: 'top', horizontal: 'right', }, })
+    }
+}
+
+export default alert
